b9347744f85601d4f0a9f47a367cf9683c4cee95
Emanuel B. <emanuelbraun1@gmail.com>
2026-09-24 17:08:15 +0200

    .

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
+echo "--disable-nan-check --no-half"
 @echo off
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api --listen --port 7880 --no-half-vae --enable-insecure-extension-access --device-id=0 --medvram --ckpt-dir "D:\2_Apps\ComfyUI App\models\checkpoints" --lora-dir "D:\2_Apps\ComfyUI App\models\loras"
+REM set COMMANDLINE_ARGS=--listen
 
 call webui.bat
